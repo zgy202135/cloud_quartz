616b395c4713d347179b9edfab9f3cb0ab928455
zhougaoyang <15751775536@163.com>
2019-12-12 10:32:57 +0800

    feat(任务调度):初始化项目

---
+++ src/main/java/com/julius/quartz/cloud/quartz/CloudQuartzApplication.java
@@ -0,0 +1,13 @@
+package com.julius.quartz.cloud.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudQuartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudQuartzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julius/quartz/cloud/quartz/CloudQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julius.quartz.cloud.quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudQuartzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
